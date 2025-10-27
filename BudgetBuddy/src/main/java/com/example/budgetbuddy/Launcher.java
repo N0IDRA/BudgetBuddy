6cd29a06f5ed3c0d38fd1a411415de3b3443bc25
N0IDRA <tamagosnicolas@gmail.com>
2025-10-28 00:02:46 +0800

    Delete BudgetBuddy directory

--- BudgetBuddy/src/main/java/com/example/budgetbuddy/Launcher.java
+++
@@ -1,9 +0,0 @@
-package com.example.budgetbuddy;
-
-import javafx.application.Application;
-
-public class Launcher {
-    public static void main(String[] args) {
-        Application.launch(BudgetBuddyApp.class, args);
-    }
-}
